50068755c3ed88154b19bfe0dbf984ef86870a77
Piotr <PiotrCzCc@users.noreply.github.com>
2016-10-10 17:00:10 +0200

    Add files via upload

--- examples/test_my_lcd/test_my_lcd.ino
+++ examples/test_my_lcd/test_my_lcd.ino
@@ -1,1 +1,17 @@
+#include <TTFLCD_ILI9325.h>
+#include <TTFLCD_ILI9325_registers.h>
 
+TFTLCD_ILI9325 MyLCD;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("TFT LCD test - START"));
+
+  uint16_t myChipID = MyLCD.ReadRegisters16(ILI9325_DRIVER_CODE_REG);
+  Serial.println(myChipID, HEX);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
